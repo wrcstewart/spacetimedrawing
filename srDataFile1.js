4d52f579542a74c00de5b75c348144f43e9563ab
wrcstewart <wrcstewart@yahoo.co.uk>
2024-02-09 12:18:09 +0000

    Initial commit

---
+++ srDataFile1.js
@@ -0,0 +1,618 @@
+/**
+ * Created by williamstewart on 25/03/2019.
+ */
+
+
+
+
+
+
+var testString00  = String.raw`
+
+{
+    "description": "DIAGRAM 0.0: Start To Explore Galilean Mode",
+    "authorEmail": "wrcstewart@yahoo.co.uk",
+    "srAppVersion": "WRCS-SR1-V0.1",
+    "appURL": "http://www.google.com",
+    "jsonStorageURL": "undefined",
+    "briefNote": "Optional brief user note",
+    "velocity": "3.51862e+6",
+    "grids": 20,
+    "mode": "Gal",
+    "gridOpacity": 0.5,
+    "gridsStyle": 0.3,
+    "srEvents": [
+        {
+            "eventLabel": "0",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 0
+            }
+        },
+        {
+            "eventLabel": "1",
+            "connects": [
+                "0",
+                "5"
+            ],
+            "pointXT": {
+                "x": 46800000,
+                "t": 3.52
+            }
+        },
+        {
+            "eventLabel": "2",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 7.2
+            }
+        },
+        {
+            "eventLabel": "3",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 6.4
+            }
+        },
+        {
+            "eventLabel": "4",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 5.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "5",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 4.8
+            }
+        },
+        {
+            "eventLabel": "6",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -4.2
+            }
+        },
+        {
+            "eventLabel": "7",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5
+            }
+        },
+        {
+            "eventLabel": "8",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5.8
+            }
+        },
+        {
+            "eventLabel": "9",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -6.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "10",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -7.4
+            }
+        }
+    ],
+    "restFrameDoNotEdit": "red"
+}
+
+
+           `;
+
+var testString10  = String.raw`
+
+{
+    "description": "DIAGRAM 1.0: Start To Explore Galilean Mode",
+    "authorEmail": "wrcstewart@yahoo.co.uk",
+    "srAppVersion": "WRCS-SR1-V0.1",
+    "appURL": "http://www.google.com",
+    "jsonStorageURL": "undefined",
+    "briefNote": "Optional brief user note",
+    "velocity": "3.51862e+6",
+    "grids": 20,
+    "mode": "Gal",
+    "gridOpacity": 0.5,
+    "gridsStyle": 0.3,
+    "srEvents": [
+        {
+            "eventLabel": "0",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 1
+            }
+        },
+        {
+            "eventLabel": "1",
+            "connects": [
+                "0",
+                "5"
+            ],
+            "pointXT": {
+                "x": 46800000,
+                "t": 3.52
+            }
+        },
+        {
+            "eventLabel": "2",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 7.2
+            }
+        },
+        {
+            "eventLabel": "3",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 6.4
+            }
+        },
+        {
+            "eventLabel": "4",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 5.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "5",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 4.8
+            }
+        },
+        {
+            "eventLabel": "6",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -4.2
+            }
+        },
+        {
+            "eventLabel": "7",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5
+            }
+        },
+        {
+            "eventLabel": "8",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5.8
+            }
+        },
+        {
+            "eventLabel": "9",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -6.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "10",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -7.4
+            }
+        }
+    ],
+    "restFrameDoNotEdit": "red"
+}
+
+
+           `;
+
+
+var testString20  = String.raw`
+
+{
+    "description": "DIAGRAM 2.0: Start To Explore Galilean Mode",
+    "authorEmail": "wrcstewart@yahoo.co.uk",
+    "srAppVersion": "WRCS-SR1-V0.1",
+    "appURL": "http://www.google.com",
+    "jsonStorageURL": "undefined",
+    "briefNote": "Optional brief user note",
+    "velocity": "3.51862e+6",
+    "grids": 20,
+    "mode": "Gal",
+    "gridOpacity": 0.5,
+    "gridsStyle": 0.3,
+    "srEvents": [
+        {
+            "eventLabel": "0",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 2
+            }
+        },
+        {
+            "eventLabel": "1",
+            "connects": [
+                "0",
+                "5"
+            ],
+            "pointXT": {
+                "x": 46800000,
+                "t": 3.52
+            }
+        },
+        {
+            "eventLabel": "2",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 7.2
+            }
+        },
+        {
+            "eventLabel": "3",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 6.4
+            }
+        },
+        {
+            "eventLabel": "4",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 5.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "5",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 4.8
+            }
+        },
+        {
+            "eventLabel": "6",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -4.2
+            }
+        },
+        {
+            "eventLabel": "7",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5
+            }
+        },
+        {
+            "eventLabel": "8",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5.8
+            }
+        },
+        {
+            "eventLabel": "9",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -6.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "10",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -7.4
+            }
+        }
+    ],
+    "restFrameDoNotEdit": "red"
+}
+
+
+           `;
+
+
+var testString01  = String.raw`
+
+{
+    "description": "DIAGRAM 0.1: Start To Explore Galilean Mode",
+    "authorEmail": "wrcstewart@yahoo.co.uk",
+    "srAppVersion": "WRCS-SR1-V0.1",
+    "appURL": "http://www.google.com",
+    "jsonStorageURL": "undefined",
+    "briefNote": "Optional brief user note",
+    "velocity": "3.51862e+6",
+    "grids": 20,
+    "mode": "Gal",
+    "gridOpacity": 0.5,
+    "gridsStyle": 0.3,
+    "srEvents": [
+        {
+            "eventLabel": "0",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 3
+            }
+        },
+        {
+            "eventLabel": "1",
+            "connects": [
+                "0",
+                "5"
+            ],
+            "pointXT": {
+                "x": 46800000,
+                "t": 3.52
+            }
+        },
+        {
+            "eventLabel": "2",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 7.2
+            }
+        },
+        {
+            "eventLabel": "3",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 6.4
+            }
+        },
+        {
+            "eventLabel": "4",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 5.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "5",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 4.8
+            }
+        },
+        {
+            "eventLabel": "6",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -4.2
+            }
+        },
+        {
+            "eventLabel": "7",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5
+            }
+        },
+        {
+            "eventLabel": "8",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5.8
+            }
+        },
+        {
+            "eventLabel": "9",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -6.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "10",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -7.4
+            }
+        }
+    ],
+    "restFrameDoNotEdit": "red"
+}
+
+
+           `;
+
+var testString02  = String.raw`
+
+{
+    "description": "DIAGRAM 0.2: Start To Explore Galilean Mode",
+    "authorEmail": "wrcstewart@yahoo.co.uk",
+    "srAppVersion": "WRCS-SR1-V0.1",
+    "appURL": "http://www.google.com",
+    "jsonStorageURL": "undefined",
+    "briefNote": "Optional brief user note",
+    "velocity": "3.51862e+6",
+    "grids": 20,
+    "mode": "Gal",
+    "gridOpacity": 0.5,
+    "gridsStyle": 0.3,
+    "srEvents": [
+        {
+            "eventLabel": "0",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 4
+            }
+        },
+        {
+            "eventLabel": "1",
+            "connects": [
+                "0",
+                "5"
+            ],
+            "pointXT": {
+                "x": 46800000,
+                "t": 3.52
+            }
+        },
+        {
+            "eventLabel": "2",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 7.2
+            }
+        },
+        {
+            "eventLabel": "3",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 6.4
+            }
+        },
+        {
+            "eventLabel": "4",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": 5.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "5",
+            "connects": [
+                "1"
+            ],
+            "pointXT": {
+                "x": 0,
+                "t": 4.8
+            }
+        },
+        {
+            "eventLabel": "6",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -4.2
+            }
+        },
+        {
+            "eventLabel": "7",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5
+            }
+        },
+        {
+            "eventLabel": "8",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -5.8
+            }
+        },
+        {
+            "eventLabel": "9",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -6.6000000000000005
+            }
+        },
+        {
+            "eventLabel": "10",
+            "connects": [],
+            "pointXT": {
+                "x": 0,
+                "t": -7.4
+            }
+        }
+    ],
+    "restFrameDoNotEdit": "red"
+}
+
+
+           `;
+
+
+
+
+
+
+var srPresets=[];
+
+
+srPresets[0] = [];
+srPresets[1] =[];
+srPresets[2] =[];
+
+srPresets[0][0] = testString00;
+srPresets[1][0] = testString10;
+srPresets[2][0] = testString20;
+
+srPresets[0][10] = testString01;
+srPresets[0][20] = testString02;
+
+
+
+//srPresets[1][1] = testString0;
